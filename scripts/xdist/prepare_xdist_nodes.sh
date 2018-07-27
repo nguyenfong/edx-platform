e986b4b72fa28e640d09bfaec801684a593e8ef5
Michael Youngstrom <myoungstrom@edx.org>
2018-07-27 17:38:33 -0400

    try without creating an ssh config

--- scripts/xdist/prepare_xdist_nodes.sh
+++ scripts/xdist/prepare_xdist_nodes.sh
@@ -9,14 +9,13 @@ python scripts/xdist/pytest_container_manager.py -a up -n ${XDIST_NUM_TASKS} \
 
 for ip in $(echo $ip_list | sed "s/,/ /g")
 do
-    container_reqs_cmd="ssh ubuntu@$ip 'cd /edx/app/edxapp/edx-platform;
+    container_reqs_cmd="ssh -o StrictHostKeyChecking=no ubuntu@$ip 'cd /edx/app/edxapp/edx-platform;
     git pull -q; git checkout -q ${XDIST_GIT_BRANCH};
     source /edx/app/edxapp/edxapp_env; pip install -qr requirements/edx/testing.txt' & "
 
     cmd=$cmd$container_reqs_cmd
 done
 cmd=$cmd"wait"
-echo "Adding ssh config"
-echo -e "Host * \n StrictHostKeyChecking no" >> ~/.ssh/config
+
 echo "Executing commmand: $cmd"
 eval $cmd
