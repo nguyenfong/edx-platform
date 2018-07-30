5d11875e62d61234257386c0f14a91cdf54736e0
Michael Youngstrom <myoungstrom@edx.org>
2018-07-30 16:52:26 -0400

    add terminate nodes script

---
+++ scripts/xdist/terminate_xdist_nodes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+echo "Terminating xdist containers with pytest_container_manager.py"
+xdist_task_arns=$(<xdist_files/${XDIST_FILE_NAME_PREFIX}_task_arns.txt)
+python scripts/xdist/pytest_container_manager.py -a down --task_arns ${xdist_task_arns}
