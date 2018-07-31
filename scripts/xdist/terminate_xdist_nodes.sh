7f105f0035ccf740055bde87b9165e104884fec2
Michael Youngstrom <myoungstrom@edx.org>
2018-07-31 14:23:54 -0400

    changes

--- scripts/xdist/terminate_xdist_nodes.sh
+++ scripts/xdist/terminate_xdist_nodes.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "Terminating xdist containers with pytest_container_manager.py"
-xdist_task_arns=$(<xdist_files/${XDIST_FILE_NAME_PREFIX}-task-arns.txt)
-python scripts/xdist/pytest_container_manager.py -a down --task_arns ${xdist_task_arns}
+
+if [ -f xdist_files/${XDIST_FILE_NAME_PREFIX}-task-arns.txt ]; then
+    echo "Terminating xdist containers with pytest_container_manager.py"
+    xdist_task_arns=$(<xdist_files/${XDIST_FILE_NAME_PREFIX}-task-arns.txt)
+    python scripts/xdist/pytest_container_manager.py -a down --task_arns ${xdist_task_arns}
+else
+    echo "File: xdist_files/${XDIST_FILE_NAME_PREFIX}-task-arns.txt not found"
+fi
